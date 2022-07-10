290055a2a02001903a4d1dc94980da40597eb265
isiratasmika257 <108164278+isiratasmika257@users.noreply.github.com>
2022-07-10 12:28:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,11 +32,11 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.owner = ['94760040313'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
 global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
 global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.pengguna = ᳆⃟⚖️ʀͥᴇᷧᴀͫʟ☬𝙸𝚂𝙸𝚁𝙰⃠⚖️' // Your name ඔබේ නම 
+global.botnma = '❤️‍🔥Drak Max❤️‍🔥' // Dark max , ඔබගෙ බොට්ගේ නම 
 global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
 global.packname = 'Queen Elisa Sticker' // Sticker package name 
 global.author = 'Dark Maker' // Sticker Autor name 
@@ -60,7 +60,7 @@ global.sendwelcome = `add`
 // Good bye එක ඔනි නම් remove කියලා දාන්න
 global.sendgoodbye = `remove`
 // Caption 
-global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐌𝐃`
+global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 ᳆⃟⚖️ʀͥᴇᷧᴀͫʟ☬𝙸𝚂𝙸𝚁𝙰⃠⚖️
 global.mess = {
     success: '✅ Done!',
     admin: 'you must be admin to use this cmd!',
